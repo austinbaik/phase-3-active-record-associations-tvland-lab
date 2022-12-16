c894745cef5459b994bd570ab9bc46d476940c7a
Austin <baik.austin@gmail.com>
2022-12-15 17:08:20 -0800

    commit tree df306b21116f1a5e93bfb1d77ba839f6459db2d4

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name 
+    # sts all of the characters that actor 
+    # has alongside the show that the character is in.
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.map do |t|
+        # t is an instance of a character 
+        "#{t.name} - #{t.show.name}"
+        # active directory extends the table so you're effectively mapping through the same table
+        end
+    end
+
 end
